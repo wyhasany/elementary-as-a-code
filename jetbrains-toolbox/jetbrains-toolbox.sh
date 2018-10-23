5a77c527625a6d0503d376c69c71ed994267d6ea
Michał Rowicki <mirowicki@gmail.com>
2018-10-23 23:32:04 +0200

    Repo init

---
+++ jetbrains-toolbox/jetbrains-toolbox.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#[ $(id -u) != "0" ] && exec sudo "$0" "$@"
+echo -e " \e[94mInstalling Jetbrains Toolbox\e[39m"
+echo ""
+
+function getLatestUrl() {
+USER_AGENT=('User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
+
+URL=$(curl 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+echo $URL
+}
+getLatestUrl
+
+FILE=$(basename ${URL})
+DEST=$PWD/$FILE
+
+echo ""
+echo -e "\e[94mDownloading Toolbox files \e[39m"
+echo ""
+wget -cO  ${DEST} ${URL} --read-timeout=5 --tries=0
+echo ""
+echo -e "\e[32mDownload complete!\e[39m"
+echo ""
+DIR="$HOME/jetbrains-toolbox"
+echo ""
+echo  -e "\e[94mInstalling to $DIR\e[39m"
+echo ""
+if mkdir ${DIR}; then
+    tar -xzf ${DEST} -C ${DIR} --strip-components=1
+fi
+
+chmod -R +rwx ${DIR}
+#touch ${DIR}/jetbrains-toolbox.sh
+#echo "#!/bin/bash" >> $DIR/jetbrains-toolbox.sh
+#echo "$DIR/jetbrains-toolbox" >> $DIR/jetbrains-toolbox.sh
+#
+#ln -s ${DIR}/jetbrains-toolbox.sh /usr/local/bin/jetbrains-toolbox
+#chmod -R +rwx /usr/local/bin/jetbrains-toolbox
+#echo ""
+rm ${DEST}
+echo  -e "\e[32mDone.\e[39m"
---
+++ zsh_files/konsole_profiles/konsole/shell.profile
@@ -0,0 +1,13 @@
+[Appearance]
+AntiAliasFonts=true
+BoldIntense=true
+ColorScheme=shell
+Font=DejaVu Sans Mono for Powerline,11,-1,5,50,0,0,0,0,0
+
+[General]
+LocalTabTitleFormat=%d : %n
+Name=shell
+Parent=FALLBACK/
+
+[Scrolling]
+HistoryMode=2
