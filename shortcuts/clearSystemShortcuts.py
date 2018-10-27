41df033ecfc37cb1cb5851faf423a0e5c0e82cf8
Grzegorz Skupien <grzegorz@skupien.net>
2018-10-27 20:11:31 +0200

    Provide in-memory XML processing

--- shortcuts/clearSystemShortcuts.py
+++ shortcuts/clearSystemShortcuts.py
@@ -5,28 +5,23 @@ import xml.etree.ElementTree as ET
 import subprocess
 import re
 
-#Download default root keymap
 url = 'https://raw.githubusercontent.com/JetBrains/intellij-community/282253b8ee888b51c0e8f63f44d9d4ecae9c19d2/platform/platform-resources/src/keymaps/%24default.xml'
-urllib.request.urlretrieve(url, "$default.xml")
-#Download XWin keymap
+default_keymappings = urllib.request.urlopen(url).read().decode("utf-8")
 url = 'https://raw.githubusercontent.com/JetBrains/intellij-community/282253b8ee888b51c0e8f63f44d9d4ecae9c19d2/platform/platform-resources/src/keymaps/Default%20for%20XWin.xml'
-urllib.request.urlretrieve(url, "Default for XWin.xml")
+xwin_mappings = urllib.request.urlopen(url).read().decode("utf-8")
 
-#Merge keymaps
-tree_parent = ET.parse('$default.xml')
+# Merge keymaps
+tree_parent = ET.ElementTree(ET.fromstring(default_keymappings))
 root_parent = tree_parent.getroot()
-
-tree_child = ET.parse('Default for XWin.xml')
+tree_child = ET.ElementTree(ET.fromstring(xwin_mappings))
 root_child = tree_child.getroot()
 
 for child in root_child:
-    #creates xpath to find parent xml element to overwrite
+    # creates xpath to find parent xml element to overwrite
     chs = root_parent.findall('.//' + child.tag + '[@id=\'' + child.attrib['id'] + '\']')
     for x in chs:
         root_parent.remove(x)
     root_parent.append(child)
-
-tree_parent.write("merged.xml")
 
 #Load all idea shortcuts to array
 idea_key_strokes = [
