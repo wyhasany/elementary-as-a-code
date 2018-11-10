5a93a4fa90cd1bfb38c5aaadbe39a3e86baaa706
Michał Rowicki <mirowicki@gmail.com>
2018-11-10 22:58:35 +0100

    Fix paths in authomatic wallpapers script

--- playbooks/wallpapers/lock.sh
+++ playbooks/wallpapers/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Pulpit
-ls -Art /home/yourusername/.config/variety/Downloaded/Bing | xargs -I{} cp /home/yourusername/.config/variety/Downloaded/Bing/{} /home/yourusername/.local/share/backgrounds/lock.jpg
+ls -Art1 /home/yourusername/.config/variety/Downloaded/Bing | tail -n 1 | xargs -I{} cp /home/yourusername/.config/variety/Downloaded/Bing/{} /usr/share/backgrounds/Ashim\ DSilva.jpg
 #Lock screen
-ls -Art /home/yourusername/.config/variety/Downloaded/Bing | xargs -I{} cp /home/yourusername/.config/variety/Downloaded/Bing/{} /var/lib/lightdm-data/yourusername/wallpaper/lock.jpg
+ls -Art1 /home/yourusername/.config/variety/Downloaded/Bing | tail -n 1 | xargs -I{} cp /home/yourusername/.config/variety/Downloaded/Bing/{} /var/lib/lightdm-data/yourusername/wallpaper/Ashim\ DSilva.jpg
 
